fc94900a9f1575e157bc82e9b8a02eb4c620d522
aliyajeylani <aliya.jeylani24@gmail.com>
2023-01-21 22:10:00 -0600

    Adding API request to pull data from OpenWeather

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,38 @@
-var requestUrl = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=e015a0a7f21ac1de523ad774a61e8902";
+// var requestUrl = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=e015a0a7f21ac1de523ad774a61e8902";
 
 
-fetch(requestUrl)
-    .then(function (response) {
 
-        return response.json();
-    })
+var citySearched = document.getElementById("city_searched");
+var searchButton = document.getElementById("search");
 
-    .then(function (data) {
-        console.log(data);
-    })
+
+
+searchButton.addEventListener("click", getForecast);
+
+
+
+
+
+function getForecast() {
+
+    var userInput = citySearched.value;
+
+    var requestUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${userInput}&appid=e015a0a7f21ac1de523ad774a61e8902`;
+
+    console.log(userInput);
+
+    fetch(requestUrl)
+        .then(function (response) {
+
+            return response.json();
+        })
+
+        .then(function (data) {
+            console.log(data)
+
+            document.getElementById("humidity").textContent = "Humdity: " + data.list[0].main.humidity;
+
+        })
+
+
+}
